913d5d6b07564a4952275deaf775b63b8f4db373
smritish21 <smritish21@gmail.com>
2025-11-03 16:25:08 +0100

    Check login status

--- offline_test_functionalities.js
+++ offline_test_functionalities.js
@@ -24,6 +24,7 @@ function extractUrlParamsAndStore() {
 }
 extractUrlParamsAndStore()
 
+//override git pull
 function checkLoginStatus(accordion){
   console.log('Checking login status')
   const accordions = document.querySelectorAll(".CoderciseList .Accordion");
